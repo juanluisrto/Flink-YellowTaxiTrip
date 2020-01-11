111a6a3e011b23e6a063b37e98b77e182105458d
Juan Luis <juanluis.rto@gmail.com>
2020-01-11 13:51:24 +0100

    tuple18 -> Event

--- src/main/java/master2019/flink/YellowTaxiTrip/LargeTrips.java
+++ src/main/java/master2019/flink/YellowTaxiTrip/LargeTrips.java
@@ -1,10 +1,30 @@
 package master2019.flink.YellowTaxiTrip;
 
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.java.tuple.Tuple18;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+
+import java.sql.Timestamp;
+
 /**
  * In this class the Large trips program has to be implemented
+ *
+ * Large trips. It reports the vendors that do 5 or more trips during 3 hours that take at least 20
+ * minutes. The output has the following format: VendorID, day, numberOfTrips,
+ * tpep_pickup_datetime, tpep_dropoff_datetime, being tpep_dropoff_datetime the time the
+ * last trip finishes and tpep_pickup_datetime the starting time of the first trip
  */
 public class LargeTrips {
-    public static void main(String[] args){
+    public static void run(SingleOutputStreamOperator<Event> mapStream){
+
+        mapStream.filter(new FilterFunction<Event>() {
+            public boolean filter(Event in) throws Exception {
+
+
+                return in.tuple18.f1.equals("sensor1");
+            }
+        });
+
 
     }
 }
--- src/main/java/master2019/flink/YellowTaxiTrip/Main.java
+++ src/main/java/master2019/flink/YellowTaxiTrip/Main.java
@@ -8,7 +8,9 @@ import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
 import java.sql.Timestamp;
+
 import org.apache.flink.api.common.functions.MapFunction;
 
 
@@ -43,13 +45,12 @@ public class Main {
         String p = "/Users/juanluisrto/Documents/Universidad/UPM/Cloud Computing/YellowTaxiTrip/yellow_tripdata_2019_06.csv";
         text = env.readTextFile(p);
 
-        SingleOutputStreamOperator<Tuple18<Integer, Timestamp,Timestamp,Integer,Float,Integer,String,Integer,Integer,Integer,Float,Float,Float,Float,Float,Float,Float,Float>> mapStream = text.
-                map(new MapFunction<String, Tuple18<Integer, Timestamp,Timestamp,Integer,Float,Integer,String,Integer,Integer,Integer,Float,Float,Float,Float,Float,Float,Float,Float>>(){
-                    public Tuple18 <Integer, Timestamp,Timestamp,Integer,Float,Integer,String,Integer,Integer,Integer,Float,Float,Float,Float,Float,Float,Float,Float>
-                        map(String in){
+        SingleOutputStreamOperator<Event> mapStream = text.
+                map(new MapFunction<String, Event>() {
+                    public Event map(String in) {
                         String[] fieldArray = in.split(",");
-                        Tuple18<Integer, Timestamp,Timestamp,Integer,Float,Integer,String,Integer,Integer,Integer,Float,Float,Float,Float,Float,Float,Float,Float>
-                            out = new Tuple18(
+                        Tuple18<Integer, Timestamp, Timestamp, Integer, Float, Integer, String, Integer, Integer, Integer, Float, Float, Float, Float, Float, Float, Float, Float>
+                                out = new Tuple18(
                                 Integer.parseInt(fieldArray[0]),    //VendorID
                                 Timestamp.valueOf(fieldArray[1]),   //tpep_pickup_datetime
                                 Timestamp.valueOf(fieldArray[2]),   //tpep_dropoff_datetime
@@ -68,12 +69,23 @@ public class Main {
                                 Float.parseFloat(fieldArray[15]),    //improvement_surcharge
                                 Float.parseFloat(fieldArray[16]),    //total_amount
                                 Float.parseFloat(fieldArray[17])    //congestion_surcharge.
-                                );
-                        return out;
+                        );
+                        return new Event(out);
                     }
                 });
 
         // execute program
         env.execute("Main");
     }
+}
+
+
+class Event {
+
+    Tuple18<Integer, Timestamp, Timestamp, Integer, Float, Integer, String, Integer, Integer, Integer, Float, Float, Float, Float, Float, Float, Float, Float> tuple18;
+
+    public Event(Tuple18<Integer, Timestamp, Timestamp, Integer, Float, Integer, String, Integer, Integer, Integer, Float, Float, Float, Float, Float, Float, Float, Float> t) {
+        tuple18 = t;
+    }
+
 }
